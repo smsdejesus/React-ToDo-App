62b58a52b8aebf91a90b3f66476f391f09fdc0d6
Shawn de Jesus <shawndj1002@gmail.com>
2025-01-01 17:42:12 -0700

    added feature to sort list by done status

--- todoapp/src/components/TodoList.jsx
+++ todoapp/src/components/TodoList.jsx
@@ -1,10 +1,19 @@
 import TodoItem from "./TodoItem";
 import styles from "./todoList.module.css";
-export default function TodoList({todos, setTodos}) {
+export default function TodoList({ todos, setTodos }) {
+  // Sort the todos by done status
+  const sortedTodos = todos
+    .slice()
+    .sort((a, b) => Number(a.done) - Number(b.done));
   return (
     <div className={styles.list}>
-    {todos.map((item) => (
-        <TodoItem key={item.name} item={item} todos={todos} setTodos={setTodos}/>
+      {sortedTodos.map((item) => (
+        <TodoItem
+          key={item.name}
+          item={item}
+          todos={todos}
+          setTodos={setTodos}
+        />
       ))}
     </div>
   );
